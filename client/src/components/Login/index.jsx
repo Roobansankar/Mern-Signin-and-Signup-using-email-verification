009bef28cae9cfa92a5cf1b426bdd6e28b8cab26
Rooban sankar <146194005+Roobansankar@users.noreply.github.com>
2024-04-28 20:58:47 +0530

    Update index.jsx

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -21,7 +21,7 @@ const Login = () => {
       const url = "http://localhost:8080/api/auth";
       const { data: res } = await axios.post(url, data);
       localStorage.setItem("token", res.data);
-      window.location = "/dashboard";
+      window.location = "/";
     } catch (error) {
       if (
         error.response &&
