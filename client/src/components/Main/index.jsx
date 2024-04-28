fa493f48d67a5ccf4e8f30dbc0f3287e473075b4
Rooban sankar <146194005+Roobansankar@users.noreply.github.com>
2024-04-29 01:47:20 +0530

    Update index.jsx

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,73 +1,74 @@
 import React, { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 
-const Main = () => {
-  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:8080/api/users");
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+const Main = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:8080/api/users");
+                const data = await response.json();
+                setUsers(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        window.location.reload();
     };
 
-    fetchData();
-  }, []);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.reload();
-  };
-
-  return (
-    <div className={styles.main_container}>
-      <nav className={styles.navbar}>
-        <h1>Email verification</h1>
-        <button className={styles.white_btn} onClick={handleLogout}>
-          Logout
-        </button>
-      </nav>
-      <div className={styles.user_list}>
-        <h2>User List</h2>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Mobile Number</th>
-              <th>Image</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <tr key={index}>
-                <td>{user.firstName}</td>
-                <td>{user.lastName}</td>
-                <td>{user.email}</td>
-                <td>{user.mobileNumber}</td>
-                <td>
-                  {user.image && (
-                    <img
-                      src={`http://localhost:8080/api/users/${user._id}/image`}
-                      alt={`${user.firstName}'s avatar`}
-                      className={styles.avatar}
-                      height="150px"
-                      width="150px"
-                    />
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.main_container}>
+            <nav className={styles.navbar}>
+                <h1>Email verification</h1>
+                <button className={styles.white_btn} onClick={handleLogout}>
+                    Logout
+                </button>
+            </nav>
+            <div className={styles.user_list}>
+                <h2>User List</h2>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Mobile Number</th>
+                            <th>Image</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user, index) => (
+                            <tr key={index}>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.email}</td>
+                                <td>{user.mobileNumber}</td>
+                                <td>
+                                    {user.image && (
+                                        <img
+                                            src={`http://localhost:8080/api/users/${user._id}/image`}
+                                            alt={`${user.firstName}'s avatar`}
+                                            className={styles.avatar}
+											height="150px"
+											width="150px"
+                                        />
+                                    )}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default Main;
